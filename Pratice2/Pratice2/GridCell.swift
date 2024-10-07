3a35c7dd14f4f8e8118d733420d83a3aaf3f7ceb
Riya22-SW <95487313+Riya22-SW@users.noreply.github.com>
2024-10-07 11:03:57 +0530

    Add files via upload

---
+++ Pratice2/Pratice2/GridCell.swift
@@ -0,0 +1,16 @@
+//
+//  GridCell.swift
+//  Pratice2
+//
+//  Created by admin on 05/10/24.
+//
+
+import UIKit
+
+class GridCell: UICollectionViewCell {
+    
+    
+    override  func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ Pratice2/Pratice2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pratice2
+//
+//  Created by admin on 05/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionview: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
